a86890387de41449d0021c4c50dcd2d3c406b6c8
Jutole <jutoleofficial@gmail.com>
2024-10-16 15:32:07 +0900

    [level 0] Title: 공배수, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/0/181936. 공배수/공배수.py
+++ 프로그래머스/0/181936. 공배수/공배수.py
@@ -1,11 +1,2 @@
 def solution(number, n, m):
-    cal_n  = number % n == 0
-    
-    if(not(cal_n)):
-        return 0
-    
-    cal_m = number % m == 0
-    if(not(cal_m)):
-        return 0
-    
-    return 1
+    return int(bool(number % n == 0) & bool(number % m == 0))
